84e6f9a87c5c267b34d9d3959d947646f7a50b3b
rongfudi636 <rongfudi636@pingan.com.cn>
2017-12-06 11:02:20 +0800

    update patterns

---
+++ patterns/lazy_evaluation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'rongfudi636'
+__mtime__ = '2017/12/6'
+"""
+
+from __future__ import print_function
+import functools
+
+
+class lazy_property(object):
+
+    def __init__(self, function):
+        self.function = function
+        functools.update_wrapper(self, function)
+
+    def __get__(self, obj, type_):
+        if obj is None:
+            return self
+        val = self.function(obj)
+        obj.__dict__[self.function.__name__] = val
+        return val
+
+
+def lazy_property2(fn):
+    attr = '_lazy__' + fn.__name__
+
+    @property
+    def _lazy_property(self):
+        if not hasattr(self, attr):
+            setattr(self, attr, fn(self))
+        return getattr(self, attr)
+    return _lazy_property
+
