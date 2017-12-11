fab84dcdbde3768fa5eae8fd4362cb50fc09f7c6
rongfudi636 <rongfudi636@pingan.com.cn>
2017-12-11 10:07:27 +0800

    add 3-tier and prototype for pattern

---
+++ patterns/3-tier.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'rongfudi636'
+__mtime__ = '2017/12/8'
+"""
+
+
+class Data(object):
+
+    """ Data Store Class """
+
+    products = {
+        'milk': {'price': 1.50, 'quantity': 10},
+        'eggs': {'price': 0.20, 'quantity': 100},
+        'cheese': {'price': 2.00, 'quantity': 10}
+    }
+
+    def __get__(self, obj, klas):
+        print("(Fetching from Data Store)")
+        return {'products': self.products}
+
+
+class BusinessLogic(object):
+
+    data = Data()
+
+    def product_list(self):
+        return self.data['products'].keys()
+
+    def product_infomation(self, product):
+        return self.data['products'].get(product, None)
+
+
+class Ui(object):
+
+    def __init__(self):
+        self.business_logic = BusinessLogic()
+
+    def get_product_list(self):
+        print('PRODUCT LIST:')
+        for product in self.business_logic.product_list():
+            print(product)
+        print("")
+
+    def get_product_infomation(self, product):
+        product_info = self.business_logic.product_infomation(product)
+        if product_info:
+            print('PRODUCT INFORMATION:')
+            print('Name: {0}, Price: {1:.2f}, Quantity: {2:}'.format(
+                product.title(), product_info.get('price', 0),
+                product_info.get('quantity', 0)))
+        else:
+            print('That product "{0}" does not exist in the records'.format(
+                product))
+
+if __name__ == '__main__':
+    ui = Ui()
+    ui.get_product_list()
+    ui.get_product_infomation('cheese')
+
---
+++ patterns/prototype.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'rongfudi636'
+__mtime__ = '2017/12/8'
+"""
+
+
+class Prototype(object):
+
+    value = 'default'
+
+    def clone(self, **attrs):
+        obj = self.__class__()
+        obj.__dict__.update(attrs)
+        return obj
+
+
+class PrototypeDispatcher(object):
+
+    def __init__(self):
+        self._objects = {}
+
+    def get_objects(self):
+        return self._objects
+
+    def register_object(self, name, obj):
+        self._objects[name] = obj
+
+    def unregister_object(self, name):
+        del self._objects[name]
+
+
+def main():
+    dispatcher = PrototypeDispatcher()
+    prototype = Prototype()
+    d = prototype.clone()
+    a = prototype.clone(value='a-value', category='a')
+    b = prototype.clone(value='b-value', is_checked=True)
+    dispatcher.register_object('objecta', a)
+    dispatcher.register_object('objectb', b)
+    dispatcher.register_object('default', d)
+    print([{n: p.value} for n, p in dispatcher.get_objects().items()])
+
+
+if __name__ == '__main__':
+    main()
